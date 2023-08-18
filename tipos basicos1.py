511b1519a40df7f37df0276e9703084db8a85fa2
Juan Fernando <juanfernandodemourasilva@gmail.com>
2023-08-18 17:05:39 -0300

    atualizando

---
+++ tipos basicos1.py
@@ -0,0 +1,12 @@
+print(int(1.4984684))
+print(int("90"))
+print(float("10.301"))
+print(float(100))
+
+valor = 100
+valor_str = str(valor)
+print(type(valor))
+print(type(valor_str))
+
+print(10 / 2)
+print(11 // 2)
