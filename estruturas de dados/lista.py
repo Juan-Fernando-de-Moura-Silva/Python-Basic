7ab70d7eacd7b69e1009c5907799e672b93f0522
Juan Fernando <juanfernandodemourasilva@gmail.com>
2023-08-19 14:06:04 -0300

    listas

---
+++ estruturas de dados/lista.py
@@ -0,0 +1,30 @@
+# Declarações de Listas #
+# exemplo 1 #
+frutas = ["laranja", "maca", "uva"]
+print(frutas)
+
+# exemplo 2 #
+frutas = []
+print(frutas)
+
+# exemplo 3 #
+letras = list("python")
+print(letras)
+
+# exemplo 4 #
+numeros = list(range(10))
+print(numeros)
+
+# exemplo 5 #
+carro = ["Ferrari", "F8", 4200000, 2020, 2900, "São Paulo", True]
+print(carro)
+
+# Assesar a Lista #
+frutas = ["laranja", "maca", "uva"]
+print(frutas[0])  # laranja
+print(frutas[2])  # uva
+
+# Tambem serve #
+
+print(frutas[-1])  # uva
+print(frutas[-2])  # laranja
