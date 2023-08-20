8c4c0128a43cfb6a7555cacd295e3f0d24a66047
Juan Fernando <juanfernandodemourasilva@gmail.com>
2023-08-20 19:36:42 -0300

    Conjuntos

---
+++ estruturas de dados/Conjuntos.py
@@ -0,0 +1,16 @@
+# Delarando conjuntos #
+
+numeros = set([1, 2, 3, 1, 3, 4])
+print(numeros)  # {1, 2, 3, 4}
+
+letras = set("abacaxi")
+print(letras)  # {"b", "a", "c", "x", "i"}
+
+carros = set(("palio", "gol", "celta", "palio"))
+print(carros)  # {"gol", "celta", "palio"}
+
+# acessando dados #
+
+numeros = list(numeros)
+
+print(numeros[0])
