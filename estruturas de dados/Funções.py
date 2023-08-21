765988bdd9b75a33e5a37fc187c48759fa4e1205
Juan Fernando <juanfernandodemourasilva@gmail.com>
2023-08-21 20:01:48 -0300

    funções

--- estruturas de dados/Funções.py
+++ estruturas de dados/Funções.py
@@ -111,3 +111,17 @@ def criar_carro(modelo, ano, placa, /, *, marca, motor, combustivel):
 # marca="Fiat", motor="1.0", combustivel="Gasolina")
 criar_carro(modelo="Palio", ano=1999, placa="ABC-1234",
             marca="Fiat", motor="1.0", combustivel="Gasolina")  # inválido
+
+# classe #
+
+
+def somar(a, b):
+    return a + b
+
+
+def exibir_resultado(a, b, funcao):
+    resultado = funcao(a, b)
+    print(f"O resultado da operação {a} + {b} = {resultado}")
+
+
+exibir_resultado(10, 10, somar)  # O resultado da operação 10 + 10 = 20
