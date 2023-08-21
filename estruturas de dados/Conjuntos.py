37cb60a3f37771a83db99f6c38d662d55b18295c
Juan Fernando <juanfernandodemourasilva@gmail.com>
2023-08-21 09:25:04 -0300

    Conjuntos atualizado

--- estruturas de dados/Conjuntos.py
+++ estruturas de dados/Conjuntos.py
@@ -14,3 +14,138 @@ print(carros)  # {"gol", "celta", "palio"}
 numeros = list(numeros)
 
 print(numeros[0])
+
+# Interando Conjuntos #
+
+for carro in carros:
+    print(carro)
+
+for indice, carro in enumerate(carros):
+    print(f"{indice}: {carro}")
+
+# Unindo #
+
+conjunto_a = {1, 2}
+conjunto_b = {3, 4}
+
+resultado = conjunto_a.union(conjunto_b)
+print(resultado)
+
+# saida{1,2,3,4} #
+
+# interserção #
+conjunto_a = {1, 2, 3}
+conjunto_b = {2, 3, 4}
+
+resultado = conjunto_a.intersection(conjunto_b)
+print(resultado)
+
+# saida {2,3} #
+
+# Diferença #
+
+resultado = conjunto_a.difference(conjunto_b)
+print(resultado)
+
+resultado = conjunto_b.difference(conjunto_a)
+print(resultado)
+
+# saida {1} & {4} #
+
+# diferença simetrica #
+
+resultado = conjunto_a.symmetric_difference(conjunto_b)
+print(resultado)
+
+# saida {1,4} #
+
+# issubset #
+
+conjunto_a = {1, 2, 3}
+conjunto_b = {4, 1, 2, 5, 6, 3}
+
+resultado = conjunto_a.issubset(conjunto_b)  # True
+print(resultado)
+
+resultado = conjunto_b.issubset(conjunto_a)  # False
+print(resultado)
+
+# issuperset #
+
+resultado = conjunto_a.issuperset(conjunto_b)  # False
+print(resultado)
+
+resultado = conjunto_b.issuperset(conjunto_a)  # True
+print(resultado)
+
+# isdisjoint #
+
+conjunto_a = {1, 2, 3, 4, 5}
+conjunto_b = {6, 7, 8, 9}
+conjunto_c = {1, 0}
+
+resultado = conjunto_a.isdisjoint(conjunto_b)  # True
+print(resultado)
+
+resultado = conjunto_a.isdisjoint(conjunto_c)  # False
+print(resultado)
+
+# add #
+
+sorteio = {1, 23}
+
+sorteio.add(25)  # {1, 23, 25}
+print(sorteio)
+
+sorteio.add(42)  # {1, 23, 25, 42}
+print(sorteio)
+
+sorteio.add(25)  # {1, 23, 25, 42}
+print(sorteio)
+
+
+# Clear #
+
+print(sorteio)  # {1, 23, 25, 42}
+
+sorteio.clear()
+
+print(sorteio)  # {}
+
+# Copia #
+
+sorteio = {2, 33}
+
+print(sorteio)  # {2, 33}
+
+sorteio.copy()
+
+print(sorteio)  # {2, 33}
+
+# discard #
+
+numeros = {1, 2, 3, 1, 2, 4, 5, 5, 6, 7, 8, 9, 0}
+
+print(numeros)  # {1, 2, 3, 4, 5, 6, 7, 8, 9, 0}
+
+numeros.discard(1)
+numeros.discard(45)
+
+print(numeros)  # {2, 3, 4, 5, 6, 7, 8, 9, 0}
+
+# pop #
+
+numeros = {1, 2, 3, 1, 2, 4, 5, 5, 6, 7, 8, 9, 0}
+
+print(numeros)  # {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+print(numeros.pop())  # 0
+print(numeros.pop())  # 1
+print(numeros)  # {2, 3, 4, 5, 6, 7, 8, 9}
+
+# remove #
+
+numeros = {1, 2, 3, 1, 2, 4, 5, 5, 6, 7, 8, 9, 0}
+
+print(numeros)  # {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+print(numeros.remove(0))  # 0
+print(numeros)  # {1, 2, 3, 4, 5, 6, 7, 8, 9}
